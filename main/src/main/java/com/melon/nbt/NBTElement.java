575c86a7f2df3c267e3a97f8c85009979ebabc80
melon_444 <1626622980@qq.com>
2025-10-08 19:49:00 +0800

    Fix bugs

--- main/src/main/java/com/melon/nbt/NBTElement.java
+++ main/src/main/java/com/melon/nbt/NBTElement.java
@@ -1,7 +1,9 @@
 package com.melon.nbt;
 
 import java.lang.reflect.Array;
+import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
 import java.nio.ByteBuffer;
 
 import com.melon.nbt.interfac3.Copyable;
@@ -108,12 +110,17 @@ public abstract class NBTElement<T> implements Copyable {
     @Override
     public NBTElement<T> copy() {
         T obj = payLoad;
+        java.lang.reflect.Type supertype = this.getClass().getGenericSuperclass();
+        Class<?> actuaType = getClassFromType(((ParameterizedType)supertype).getActualTypeArguments()[0]);
+
+        //System.out.println("supertype: "+supertype+"\nactuaType: "+actuaType);
+
         try {
 
             // Don't copy box type and String
             if (obj instanceof Number || obj instanceof String || obj instanceof Boolean
                     || obj instanceof Character) {
-                return (NBTElement<T>) this.getClass().getDeclaredConstructor(String.class, payLoad.getClass())
+                return (NBTElement<T>) this.getClass().getDeclaredConstructor(String.class, actuaType)
                         .newInstance(keyName, obj);
             }
 
@@ -131,7 +138,7 @@ public abstract class NBTElement<T> implements Copyable {
                         Array.set(copy, i, m.invoke(obj));
                     }
                 }
-                return (NBTElement<T>) this.getClass().getDeclaredConstructor(String.class, payLoad.getClass())
+                return (NBTElement<T>) this.getClass().getDeclaredConstructor(String.class, actuaType)
                         .newInstance(keyName, (T) copy);
             }
 
@@ -149,25 +156,59 @@ public abstract class NBTElement<T> implements Copyable {
                         copy.add(m.invoke(obj));
                     }
                 }
-                return (NBTElement<T>) this.getClass().getDeclaredConstructor(String.class, payLoad.getClass())
+                return (NBTElement<T>) this.getClass().getDeclaredConstructor(String.class, actuaType)
                         .newInstance(keyName, (T) copy);
             }
 
             // if it use Copyable interface,use the method itself has.
             if (obj instanceof Copyable c) {
-                return (NBTElement<T>) this.getClass().getDeclaredConstructor(String.class, payLoad.getClass())
+                return (NBTElement<T>) this.getClass().getDeclaredConstructor(String.class, actuaType)
                         .newInstance(keyName, c.copy());
             }
 
             // else try clone()
             Method m = obj.getClass().getDeclaredMethod("clone");
             m.setAccessible(true);
-            return (NBTElement<T>) this.getClass().getDeclaredConstructor(String.class, payLoad.getClass())
+            return (NBTElement<T>) this.getClass().getDeclaredConstructor(String.class, actuaType)
                     .newInstance(keyName, (T) m.invoke(obj));
         } catch (Exception e) {
             // fallback
             throw new UnsupportedOperationException("Cannot copy " + obj.getClass(), e);
         }
     }
+
+        private static Class<?> getClassFromType(java.lang.reflect.Type type) {
+        if (type instanceof Class<?> cls) {
+            return cls; // 普通类
+        } else if (type instanceof ParameterizedType pt) {
+            // 泛型类型，取原始类型
+            java.lang.reflect.Type rawType = pt.getRawType();
+            if (rawType instanceof Class<?> rawCls) {
+                return rawCls;
+            }
+        } else if (type instanceof GenericArrayType gat) {
+            // 泛型数组，递归获取数组元素类型
+            Class<?> componentClass = getClassFromType(gat.getGenericComponentType());
+            if (componentClass != null) {
+                return Array.newInstance(componentClass, 0).getClass();
+            }
+        } else if (type instanceof java.lang.reflect.TypeVariable<?> tv) {
+            // 类型变量，尝试获取上界（默认 Object）
+            java.lang.reflect.Type[] bounds = tv.getBounds();
+            if (bounds.length > 0) {
+                return getClassFromType(bounds[0]);
+            }
+        } else if (type instanceof java.lang.reflect.WildcardType wt) {
+            // 通配符 ? extends X
+            java.lang.reflect.Type[] upperBounds = wt.getUpperBounds();
+            if (upperBounds.length > 0) {
+                return getClassFromType(upperBounds[0]);
+            }
+        }
+
+        // 无法确定
+        return null;
+    }
+
 
 }
